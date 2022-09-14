b147d71952388256a5bb76eb848ece8bc2221410
mmhasan01 <91401851+Hasa0891@users.noreply.github.com>
2022-09-14 12:54:08 +0000

    visual animation simple

---
+++ visual/animations/simple.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as a
+import random
+
+fig, ax = plt.subplots()
+
+def animate(f):
+    colours = ["y", "b", "g", "r"]
+    color = colours[random.randint(0, 3)]
+    ax.set_ylim(0,10) 
+    ax.set_xlim(0,10)
+    ax.plot(f, f, color +"o")
+
+def run():
+    fred = a.FuncAnimation(fig, animate, interval = 1000, frames = 10)
+    plt.show()
+
+run()
